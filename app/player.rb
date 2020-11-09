2a7522862e42e4abb9deb8e99f7e436e07fdc50f
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-11-09 11:16:08 -0500

    add 'flip' property

--- app/player.rb
+++ app/player.rb
@@ -16,11 +16,18 @@ class Player < Entity
 
   def move_right
     # todo sprite direction
+    self.flip = false
     self.x += SPEED if self.x + self.w < $WIDTH
   end
 
   def move_left
     # todo sprite direction
+    self.flip = true
     self.x -= SPEED if self.x > 0
   end
+
+  def draw
+    [x, y, w, h, sprite_path, 0, 255, 255, 255, 0, 0, 0, -1, -1, self.flip, false,  0.5, 1.0]
+  end
+
 end

98e1829ef67963ce0aca1af285e8993f4a0618cb
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-11-09 12:22:11 -0500

    fix sprite color and add comments

--- app/player.rb
+++ app/player.rb
@@ -8,24 +8,35 @@ class Player < Entity
     super($WIDTH/2-w/2, 150, w, 80, "sprites/dragon-0.png", false)
   end
 
-  def calc
+  # def calc
 
-  end
+  # end
 
   def move_right
-    # todo sprite direction
     self.flip = false
     self.x += SPEED if self.x + self.w < $WIDTH
   end
 
   def move_left
-    # todo sprite direction
     self.flip = true
     self.x -= SPEED if self.x > 0
   end
 
   def draw
-    [x, y, w, h, sprite_path, 0, 255, 255, 255, 0, 0, 0, -1, -1, self.flip, false,  0.5, 1.0]
+    [
+      x, y, w, h, sprite_path,
+      0,              # ANGLE
+      255,            # ALPHA
+      255,            # RED SATURATION
+      255,            # GREEN SATURATION
+      255,            # BLUE SATURATION
+      0,              # TILE X
+      0,              # TILE Y
+      self.w,         # TILE W
+      self.h,         # TILE H
+      self.flip,      # FLIP HORIZONTALLY
+      false           # FLIP VERTICALLY
+    ]
   end
 
 end

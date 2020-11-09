3f234f50b4dbecd4c2ab37237ac4ec1a5db7f5a7
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-11-09 16:54:48 -0500

    placeholder beam

--- app/player.rb
+++ app/player.rb
@@ -1,7 +1,7 @@
 require 'app/entity.rb'
 
 class Player < Entity
-  attr_accessor :total_ghosts_held, :ghost_limit
+  attr_accessor :total_ghosts_held, :ghost_limit, :beam, :is_shooting
   SPEED = 4
 
   def initialize
@@ -9,6 +9,8 @@ class Player < Entity
     super($WIDTH/2-w/2, 150, w, 80, "sprites/dragon-0.png", false)
     @total_ghosts_held = 0
     @ghost_limit = 10 # max amount of ghosts that can be stored in the backpack
+    @beam = {x: (self.x + self.w/2).to_i, y: self.y+60, h: 300, w: 10}
+    @is_shooting = false
   end
 
   # def calc
@@ -29,22 +31,38 @@ class Player < Entity
     self.total_ghosts_held += 1 if self.total_ghosts_held < self.ghost_limit
   end
 
-  # def draw
-  #   [
-  #     x, y, w, h, sprite_path,
-  #     0,              # ANGLE
-  #     255,            # ALPHA
-  #     255,            # RED SATURATION
-  #     255,            # GREEN SATURATION
-  #     255,            # BLUE SATURATION
-  #     0,              # TILE X
-  #     0,              # TILE Y
-  #     self.w,         # TILE W
-  #     self.h,         # TILE H
-  #     self.flip,      # FLIP HORIZONTALLY
-  #     false           # FLIP VERTICALLY
-  #   ]
-  # end
+  def shoot(args)
+    # self.beam.y += 3
+
+    # center beam
+    self.beam.x = self.x + self.w/2
+
+    # placeholder beam
+    args.outputs.sprites << [self.beam.x, self.beam.y, self.beam.w, self.beam.h, 'sprites/beam.png']
+
+
+    # debug
+    args.outputs.labels << [10, $HEIGHT - 20, "#{self.beam.x}, #{self.beam.y}", 255, 255, 255]
+  end
+
+  def render(args)
+    self.shoot(args)
+
+    [
+      x, y, w, h, sprite_path,
+      0,              # ANGLE
+      self.alpha,            # ALPHA
+      255,            # RED SATURATION
+      255,            # GREEN SATURATION
+      255,            # BLUE SATURATION
+      0,              # TILE X
+      0,              # TILE Y
+      self.w,         # TILE W
+      self.h,         # TILE H
+      self.flip,      # FLIP HORIZONTALLY
+      false           # FLIP VERTICALLY
+    ]
+  end
 
 
   def render_ui args

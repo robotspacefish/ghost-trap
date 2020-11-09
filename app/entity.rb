7373533c48c0efdba267dd7b2ff82c57cdaa3762
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-11-09 11:15:30 -0500

    add recommended methods in case of an exception

--- app/entity.rb
+++ app/entity.rb
@@ -15,6 +15,20 @@ class Entity
   end
 
   def draw
-    [x, y, w, h, sprite]
+    [x, y, w, h, sprite_path]
   end
+
+  # override :serialize and return hash to Class can be persisted to disk in the event of an exception
+  def serialize
+    { sprite_path: sprite_path, flip: flip }
+  end
+
+  def inspect
+    serialize.to_s
+  end
+
+  def to_s
+    serialize.to_s
+  end
+
 end

6e56ef502e81fa632eb3e4572b24076bf1062b8f
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-11-17 19:54:42 -0500

    change disposal open status & sprite if player is colliding

--- app/disposal.rb
+++ app/disposal.rb
@@ -1,7 +1,7 @@
 require 'app/entity.rb'
 
 class Disposal < Entity
-  attr_accessor :total_ghosts
+  attr_accessor :total_ghosts, :is_open
 
   def initialize(y)
     super(50, y, 80, 80, 'sprites/hexagon-blue.png', false)
@@ -12,4 +12,16 @@ class Disposal < Entity
   def deposit_ghosts(total_to_add)
     self.total_ghosts += total_to_add
   end
+
+  def calc(player)
+    self.is_open = self.is_colliding_with?(player)
+  end
+
+  def render
+    self.sprite_path = self.is_open ? 'sprites/hexagon-orange.png' : 'sprites/hexagon-blue.png'
+
+    super
+  end
 end
+
+# method to change is_open to true/false when colliding with player
